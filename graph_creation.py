2536524e2a18206c87e8d3bbaaf82c46d8771eb7
cbelitz <clarabelitz1@gmail.com>
2021-04-04 14:18:34 -0400

    code to generate graphs

---
+++ graph_creation.py
@@ -0,0 +1,160 @@
+import pandas as pd
+from scipy.stats import pearsonr
+import scipy
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import numpy as np
+import random
+
+# replace with names of datasets. Can incorporate path structure if desired
+DATASETS = [
+    'fairfs_results_synthetic_auc_2.csv',
+    'fairfs_results_adult_auc_2.csv',
+    'data_auc_march_2019/fairfs_results_uci_student_academics_auc_2.csv',
+    'fairfs_results_uci_student_performance_math_auc_2.csv',
+    'fairfs_results_uci_student_performance_portuguese_auc_2.csv'
+]
+
+# Titles in same order as datasets
+TITLES = [
+    'Simulated Data',
+    'Adult',
+    'Student Academics',
+    'Student Performance (Math)',
+    'Student Performance (Portuguese)'
+]
+
+DATA_DICT = {}
+
+for i in range(len(DATASETS)):
+    DATA_DICT[TITLES[i]] = DATASETS[i]
+
+
+def create_data_arrays(sheet_name):
+    results = pd.read_csv(sheet_name, sep=',')
+    return results
+
+
+def create_plot(title, results, location):
+    x = list(range(0, 5))
+    title = title
+    plt.title(title)
+    plt.xlabel("Unfairness Weight")
+    plt.ylabel("Result (Mean Over 100 Trials)")
+
+    # overall accuracy
+    plt.plot(x, results[0:5], ':o', color="orange", label="Overall Accuracy")
+    overall = Line2D([0], [0], color='orange', linestyle=":", label="Overall Accuracy")
+
+    # statistical parity
+    plt.plot(x, results[5:10], '-o', color="black", label="Statistical Parity")
+    statistical = Line2D([0], [0], color='black', linestyle="-", label="Statistical Parity")
+
+    # conditional procedure
+    plt.plot(x, results[10:15], '--o', color='red', label="Conditional Procedure")
+    condition = Line2D([0], [0], color='red', linestyle="--", label="Conditional Procedure")
+
+    # conditional use accuracy equality
+    plt.plot(x, results[15:20], '-.o', color='green', label="Conditional Use Accuracy")
+    use_accuracy = Line2D([0], [0], color='green', linestyle="-.", label="Conditional Use Accuracy")
+
+    # treatment equality
+    plt.plot(x, results[20:25], '--o', color="cyan", label="Treatment Equality")
+    treatment = Line2D([0], [0], color='cyan', linestyle="--", label="Treatment Equality")
+
+    # all equality
+    plt.plot(x, results[25:30], '-o', color='blue', label="Total Average Equality")
+    total = Line2D([0], [0], color='blue', linestyle="-", label="Total Average Equality")
+
+    plt.legend(handles=[total, condition, use_accuracy, overall, statistical, treatment], loc=location, prop={'size': 8})
+    plt.savefig(title + '.pdf', dpi=200)
+    plt.clf()
+
+
+def make_all_unfairness_plots():
+    for key, value in DATA_DICT.items():
+        results = create_data_arrays(value)
+        # calculate mean unfairness using unfairness column
+        means = calc_mean(results, 'unfairness')
+        create_plot("Gaussian NB - " + key + " - Unfairness", means[0:30], 'upper right')
+        create_plot("Logistic Regression - " + key + " - Unfairness", means[30:60], 'upper right')
+        create_plot("Decision Tree - " + key + " - Unfairness", means[60:90], 'upper right')
+
+
+def make_all_accuracy_plots():
+    for key, value in DATA_DICT.items():
+        results = create_data_arrays(value)
+        # calculate mean accuracy using AUC column
+        means = calc_mean(results, 'auc')
+        create_plot("Gaussian NB - " + key + " - Accuracy", means[0:30], 'lower left')
+        create_plot("Logistic Regression - " + key + " - Accuracy", means[30:60], 'lower left')
+        create_plot("Decision Tree - " + key + " - Accuracy", means[60:90], 'lower left')
+
+
+def calc_mean(results, col):
+    means = []
+    # hard coded for current number of experiments
+    # 100 iterations per unfairness weight + method and 9000 total columns per dataset
+    # change if needed
+    for i in range(0, 9000, 100):
+        data = results[col][i:i + 99]
+        mean = scipy.mean(data)
+        means.append(mean)
+    return means
+
+
+def all_correlation():
+    correlations = {}
+    for key, value in DATA_DICT.items():
+        results = create_data_arrays(value)
+        correlations[key] = calc_correl(results)
+
+    return correlations
+
+
+def calc_correl(results):
+    # hard coded for current number of experiments
+    # 500 total iterations per method and 9000 total columns per dataset
+    # change if needed
+    correlations = []
+    for i in range(0, 9000, 500):
+        auc = results['auc'][i:i + 499]
+        unfairness = results['unfairness'][i:i + 499]
+        corr, p = pearsonr(auc, unfairness)
+        correlations.append({
+            'model': results['model'][i],
+            'metric': results['unfairness_metric'][i],
+            'correlation': corr,
+            'p-value': p
+        })
+
+    return correlations
+
+
+def plot_correlations():
+    results = create_data_arrays(DATA_DICT['Student Academics'])
+    # scatter plot a few correlations as an example.
+    # randomly select indices to plot (can hardcode if desired)
+    # indices = [2000, 5500, 7000]
+    indices = random.sample(range(0, 9000, 500), 3)
+    for i in indices:
+        auc = results['auc'][i:i + 499]
+        unfairness = results['unfairness'][i:i + 499]
+        plt.scatter(auc, unfairness, color='gray')
+        # fit linear regression line
+        m, b = np.polyfit(auc, unfairness, 1)
+        plt.plot(auc, m * auc + b, color='black')
+        model = results['model'][i]
+        metric = results['unfairness_metric'][i]
+        plt.xlabel("Accuracy")
+        plt.ylabel("Unfairness")
+        plt.title('Student Academics Correlation')
+        plt.savefig(str(i) + model + '_' + metric + '_student_academics.pdf', dpi=200)
+        plt.clf()
+
+
+if __name__ == '__main__':
+    make_all_accuracy_plots()
+    make_all_unfairness_plots()
+    # all_correlation()
+    # plot_correlations()
