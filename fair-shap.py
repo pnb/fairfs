d1215ec4f3f4f89d9e9c85be3d74a900d6494bb4
cbelitz <clarabelitz1@gmail.com>
2022-01-22 18:19:36 -0600

    added main function

--- fair-shap.py
+++ fair-shap.py
@@ -21,14 +21,8 @@ COLUMNS_SYNTH = ['group', 'fair_feature', 'unfair_feature']
 COLUMNS_ADULT = ['Age', 'Workclass', 'Education-Num', 'Marital Status',
                  'Occupation', 'Relationship', 'Race', 'Sex', 'Capital Gain',
                  'Capital Loss', 'Hours per week', 'Country']
-
-# Adult dataset (comes pre-cleaned in shap library)
-X, y = shap.datasets.adult()
-columns = X.columns
-
-# Simulated dataset
-# ds = dataset_loader.get_simulated_data()['simulated_data']
-# X, y = ds['data'], pd.Series(ds['labels'])
+STAT_PARITY_COLUMNS_SYNTH = ['group 1', 'group 0', 'ratio']
+STAT_PARITY_COLUMNS_ADULT = ['male', 'female', 'ratio']
 
 
 # Get average of values in dictionary
@@ -107,8 +101,57 @@ def marginal_dist(truth, predict):
 
 
 def treatment_score(truth, predict):
-    """ Calculate treatement score (here using ration of false neg to false pos)
-    """
+    """ Calculate treatement score (here using ration of false neg to false pos)"""
     fn_index = [1 for i in range(len(predict)) if truth[i] != predict[i] if truth[i] == 0]
     fp_index = [1 for i in range(len(predict)) if truth[i] != predict[i] if truth[i] == 1]
     return len(fn_index) / len(fp_index)
+
+
+def stat_parity(data, labels, cols, shap_values):
+    """ Calculate marginal distribution for priv and unpriv group, then find ratio.
+    Return a dataframe with all three values per column (feature)
+    """
+    all_marginal_dist = pd.DataFrame(index=STAT_PARITY_COLUMNS_ADULT, columns=cols)
+    converted_df = convert_shap_to_bools(data, shap_values)
+
+    privileged_indices = (pd.DataFrame((
+        [data.loc[X[PROTECTED_COLUMN] == PRIVILEGED_VALUE]])[0])).index
+    privileged_labels = labels[privileged_indices]
+
+    unprivileged_indices = (pd.DataFrame((
+        [data.loc[X[PROTECTED_COLUMN] == UNPRIVILEGED_VALUE]])[0])).index
+    unprivileged_labels = labels[unprivileged_indices]
+
+    for col in cols:
+        all_marginal_dist[col]['male'] = marginal_dist(privileged_labels,
+                                                       converted_df[col].
+                                                       iloc[privileged_indices].
+                                                       tolist())
+        all_marginal_dist[col]['female'] = marginal_dist(unprivileged_labels,
+                                                         converted_df[col].
+                                                         iloc[unprivileged_indices].
+                                                         tolist())
+        all_marginal_dist[col]['ratio'] = all_marginal_dist[col]['male'] / all_marginal_dist[col]['female']
+
+    return all_marginal_dist
+
+
+if __name__ == "__main__":
+    # Adult dataset (comes pre-cleaned in shap library)
+    X, y = shap.datasets.adult()
+    columns = X.columns
+
+    # Simulated dataset
+    # ds = dataset_loader.get_simulated_data()['simulated_data']
+    # X, y = ds['data'], pd.Series(ds['labels'])
+
+    accuracy, shap_values, pred_labels = run_model(X, y)
+
+    # results = pd.DataFrame({
+    #     'shap_values': shap_values,
+    #     'pred_labels': pred_labels
+    # })
+
+    # accuracy.to_csv('fairfs_shap_accuracy.csv', index=False, encoding='utf-8') #to do: fix
+    shap_values.to_csv('fairfs_shap_results.csv', index=False, encoding='utf-8')
+    pred_labels.to_csv('fairfs_shap_labels.csv', index=False, encoding='utf-8')
