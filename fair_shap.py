778d791c9709d0a1a54c7474730d8909caa484d5
cbelitz <clarabelitz1@gmail.com>
2022-11-30 18:56:51 -0600

    some code cleanup in fair shap file, fixes for properly tracking feature selection and using the synthetic dataset

--- fair_shap.py
+++ fair_shap.py
@@ -6,11 +6,15 @@ from tqdm import tqdm
 from sklearn import metrics, model_selection, pipeline
 from sklearn import tree, linear_model, naive_bayes
 import unfairness_metrics
+import dataset_loader
 
 from column_threshold_selector import ColumnThresholdSelector
 
-FILENAME = 'fairfs_shap_results_11302022.csv'
-PROTECTED_COLUMN = 'Sex'  # 'Sex' for adult, 'group' for synthetic
+PROTECTED_COLUMN = 'group'  # 'Sex' for adult, 'group' for synthetic
+DATASET = 'synthetic'  # options currently adult or synthetic
+FILENAME_STR = 'fairfs_shap_results_11302022_synthetic'
+FILENAME = FILENAME_STR + '.csv'
+FEATURE_FILENAME = FILENAME_STR + '_selected_features.csv'
 PRIVILEGED_VALUE = 1      # 1 for synthetic and for adult (indicates male)
 UNPRIVILEGED_VALUE = 0    # 0 for synthetic and for adult (indicates female)
 ITERATIONS = 100
@@ -19,7 +23,6 @@ ACCURACY_METRIC = metrics.roc_auc_score
 #               tree.DecisionTreeClassifier(random_state=11798)]
 MODEL_LIST = [tree.DecisionTreeClassifier(random_state=11798)]
 UNFAIRNESS_METRICS_LIST = unfairness_metrics.UNFAIRNESS_METRICS
-SELECTION_METRIC = 'treatment_equality'  # change as desired
 SELECTION_CUTOFFS = [.1, .2, .4, .8]
 
 
@@ -31,12 +34,26 @@ def main():
     except FileNotFoundError:
         pass
 
-    X, y_tmp = shap.datasets.adult()
-    y = pd.Series(y_tmp, index=X.index)
+    try:
+        feature_selection_dfs.append(pd.read_csv(FEATURE_FILENAME))
+    except FileNotFoundError:
+        pass
+
+    if DATASET == 'adult':
+        X, y_tmp = shap.datasets.adult()
+        y = pd.Series(y_tmp, index=X.index)
+
+    if DATASET == 'synthetic':
+        ds = dataset_loader.get_simulated_data()['simulated_data']
+        X = pd.DataFrame(ds['data'], columns=ds['feature_names'])
+        y = pd.Series(ds['labels'])
+        SELECTION_CUTOFFS = [.4, .8]  # only 3 columns, so values smaller than .4 will select no features
+
+    else:
+        print("Please select which dataset you are using")
 
     # Pick the column(s) of interest to use as the group labels
     group_membership = X[PROTECTED_COLUMN]
-    # protected_col_index = np.nonzero(X['feature_names'] == PROTECTED_COLUMN)[0][0]
 
     for m in MODEL_LIST:
         for unfairness_metric in UNFAIRNESS_METRICS_LIST:
@@ -58,14 +75,6 @@ def main():
                                                                             unfairness_metric,
                                                                             selection_cutoff
                                                                             )
-                # unfairnesses, aucs = run_experiment(X,
-                #                                     y,
-                #                                     m,
-                #                                     group_membership,
-                #                                     PRIVILEGED_VALUE,
-                #                                     unfairness_metric,
-                #                                     selection_cutoff
-                #                                     )
 
                 dfs.append(pd.DataFrame({
                     'model': [m.__class__.__name__] * len(aucs),
@@ -75,11 +84,9 @@ def main():
                     'unfairness': unfairnesses,
                     'auc': aucs,
                 }))
-                feature_selection_dfs.append(pd.DataFrame({
-                    'protected_column_selected_prop': selected_feature_props.T,
-                }))
                 pd.concat(dfs).to_csv(FILENAME, index=False)
-                pd.concat(feature_selection_dfs).to_csv(FILENAME+'_selected_features', index=False)
+                feature_selection_dfs.append(selected_feature_props)
+                pd.concat(feature_selection_dfs).to_csv(FEATURE_FILENAME, index=False)
 
 
 def run_experiment(X, y, model, group_membership, privileged_value, unfairness_metric, selection_cutoff):
@@ -91,9 +98,9 @@ def run_experiment(X, y, model, group_membership, privileged_value, unfairness_m
     # Create lists to hold fairness and accuracy for each run
     unfairness_means = []
     auc_means = []
-    selected_feature_props = pd.DataFrame(data=np.zeros([X.shape[1], ITERATIONS]),
-                                          index=X.columns)
-
+    selected_feature_props = pd.DataFrame(data=np.zeros([ITERATIONS, X.shape[1]]),
+                                          columns=X.columns
+                                          )
     for i in tqdm(range(ITERATIONS), desc=' Training ' + model.__class__.__name__):
         # Create 10-fold cross-validation train test split for the overall model
         # TODO: see if doing 4-fold helps with speed up
@@ -118,11 +125,9 @@ def run_experiment(X, y, model, group_membership, privileged_value, unfairness_m
         auc_means.append(result['test_auc'].mean())
         for estimator in result['estimator']:
             for feature_i in estimator.named_steps['feature_selection'].selected_features:
-                selected_feature_props[i][feature_i] += 1 / len(result['estimator'])
+                selected_feature_props[feature_i][i] += 1 / len(result['estimator'])
 
-    # print(selected_feature_props)
     return unfairness_means, auc_means, selected_feature_props
-    # return unfairness_means, auc_means
 
 
 if __name__ == "__main__":
